2737a0b3ebead9a07177b53ca74372cdc41862fc
JohanGuerra12 <167925229+JohanGuerra12@users.noreply.github.com>
2024-08-19 20:35:54 -0500

    Funcion mostrar informacion del producto

--- src/main/java/MiTecho/MiTecho/controller/HomeController.java
+++ src/main/java/MiTecho/MiTecho/controller/HomeController.java
@@ -1,17 +1,24 @@
 package MiTecho.MiTecho.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import MiTecho.MiTecho.model.Producto;
 import MiTecho.MiTecho.service.ProductoService;
-import aj.org.objectweb.asm.Attribute;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -21,4 +28,15 @@ public class HomeController {
 		model.addAttribute("productos",productoService.findAll());
 		 return "usuario/home";
 	 }
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id , Model model) {
+		log.info("Id product enviado como parametro {}", id);
+		Producto producto =  new Producto();	
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		return "usuario/productohome";
+	}
 }
