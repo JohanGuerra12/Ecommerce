f75b7849300b102fa53263277f9897b22121e763
JohanGuerra12 <167925229+JohanGuerra12@users.noreply.github.com>
2024-08-19 21:31:53 -0500

    Mapeo de carrito  y configuracion de ruta

--- src/main/java/MiTecho/MiTecho/controller/HomeController.java
+++ src/main/java/MiTecho/MiTecho/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import MiTecho.MiTecho.model.Producto;
@@ -39,4 +40,9 @@ public class HomeController {
 		model.addAttribute("producto", producto);
 		return "usuario/productohome";
 	}
+	@PostMapping("/cart")
+	public String cart() {
+		return "usuario/carrito";
+	}
+
 }
